850792ca5448a86a83f5400f73c7b0695c3f8b4c
tatsuma-sato <t.tsuma.s@gmail.com>
2022-01-03 12:05:16 -0800

    added features

---
+++ src/data.js
@@ -0,0 +1,66 @@
+// {
+//   results: [
+//   {
+//   "gender": "female",
+//   "name": {
+//   "title": "Miss",
+//   "first": "Margaux",
+//   "last": "Guerin"
+//   },
+//   "location": {
+//   "street": {
+//   "number": 8485,
+//   "name": "Rue Jean-Baldassini"
+//   },
+//   "city": "Pau",
+//   "state": "Val-de-Marne",
+//   "country": "France",
+//   "postcode": 82367,
+//   "coordinates": {
+//   "latitude": "-3.0752",
+//   "longitude": "107.8987"
+//   },
+//   "timezone": {
+//   "offset": "-6:00",
+//   "description": "Central Time (US & Canada), Mexico City"
+//   }
+//   },
+//   "email": "margaux.guerin@example.com",
+//   "login": {
+//   "uuid": "b623debe-c273-40e4-ab8b-fef2179c432b",
+//   "username": "silverzebra911",
+//   "password": "lewis",
+//   "salt": "jBob5qzU",
+//   "md5": "c7229209f71c7b35910d064dd6c378a9",
+//   "sha1": "43d8e8871eb9eb1c3becb935e3da8908ef5de444",
+//   "sha256": "537791ba1f7c8789c031eb15c35d3096b3acda9294d71ddef558813c1b2fd790"
+//   },
+//   "dob": {
+//   "date": "1995-08-09T16:02:37.516Z",
+//   "age": 27
+//   },
+//   "registered": {
+//   "date": "2005-06-11T16:49:36.234Z",
+//   "age": 17
+//   },
+//   "phone": "03-67-32-17-24",
+//   "cell": "06-68-45-22-69",
+//   "id": {
+//   "name": "INSEE",
+//   "value": "2NNaN51853492 20"
+//   },
+//   "picture": {
+//   "large": "https://randomuser.me/api/portraits/women/9.jpg",
+//   "medium": "https://randomuser.me/api/portraits/med/women/9.jpg",
+//   "thumbnail": "https://randomuser.me/api/portraits/thumb/women/9.jpg"
+//   },
+//   "nat": "FR"
+//   }
+//   ],
+//   "info": {
+//   "seed": "5cd662bbca2d279f",
+//   "results": 1,
+//   "page": 1,
+//   "version": "1.3"
+//   }
+// }
